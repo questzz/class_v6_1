b51cab439727a519df0407823f3470b989b1cbcf
ITPS <ITPS@gmail.com>
2022-02-22 15:19:53 +0900

    'init2'

--- app/src/main/java/com/example/mylottoapp/MainActivity.java
+++ app/src/main/java/com/example/mylottoapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.mylottoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -21,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private Button initButton;
     private Button runButton;
     private Set<Integer> pickerNumberSet = new HashSet<>();
+    private ArrayList<TextView> textViews = new ArrayList<>();
+    private boolean didRun = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,22 +46,69 @@ public class MainActivity extends AppCompatActivity {
         initButton = findViewById(R.id.initButton);
         runButton = findViewById(R.id.runButton);
 
+        textViews.add(findViewById(R.id.textView1));
+        textViews.add(findViewById(R.id.textView2));
+        textViews.add(findViewById(R.id.textView3));
+        textViews.add(findViewById(R.id.textView4));
+        textViews.add(findViewById(R.id.textView5));
+        textViews.add(findViewById(R.id.textView6));
     }
 
     private void addEventListener() {
         addButton.setOnClickListener(v -> {
-            //Log.d("TAG", numberPicker.getValue() + "");
-            if(pickerNumberSet.size() <= 5) {
-                pickerNumberSet.add(numberPicker.getValue());
+
+            int selectedNumber = numberPicker.getValue();
+
+            if (didRun) {
+                Toast.makeText(this, "초기화 후에 시도해주세요", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (pickerNumberSet.size() >= 5) {
+                Toast.makeText(this, "번호는 5개까지만 선택 가능합니다.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (pickerNumberSet.contains(selectedNumber)) {
+                Toast.makeText(this, "이미 선택한 번호 입니다.", Toast.LENGTH_SHORT).show();
+                return;
             }
 
+            TextView textView = textViews.get(pickerNumberSet.size());
+            textView.setVisibility(View.VISIBLE);
+            textView.setText(String.valueOf(selectedNumber));
+            textView.setBackground(setTextViewBackground(selectedNumber));
+            pickerNumberSet.add(selectedNumber);
         });
-        initButton.setOnClickListener(v -> {
 
+        initButton.setOnClickListener(v -> {
+            didRun = false;
+            pickerNumberSet.clear();
+            for (TextView tv : textViews) {
+                tv.setVisibility(View.GONE);
+            }
         });
+
         runButton.setOnClickListener(v -> {
-//            getRandomNumberTest();
-            getRandomNumber();
+
+            if (didRun) {
+                Toast.makeText(this, "초기화 후에 시도해 주세요",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            List<Integer> list = getRandomNumber();
+            list.addAll(pickerNumberSet);
+            // 코드 결과 상태 ( 6개 난수가 저장 됨) --> ( list )
+            Collections.sort(list);
+            Log.d("TAG", list.toString());
+
+            for (int i = 0; i < list.size(); i++) {
+                textViews.get(i).setText(String.valueOf(list.get(i))); //
+                textViews.get(i).setVisibility(View.VISIBLE);
+                textViews.get(i).setBackground(setTextViewBackground(list.get(i)));
+            }
+
+            didRun = true;
+
         });
     }
 
@@ -64,8 +118,8 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Integer> list = new ArrayList<>();
         while (list.size() < 6) {
             int number = random.nextInt(45) + 1;
-            if(list.contains(number)) {
-               continue;
+            if (list.contains(number)) {
+                continue;
             }
             list.add(number);
         }
@@ -80,14 +134,30 @@ public class MainActivity extends AppCompatActivity {
         Log.d("TAG", pickerNumberSet.toString());
 
         for (int i = 1; i < 46; i++) {
-            if(pickerNumberSet.contains(i)) {
+
+            if (pickerNumberSet.contains(i)) {
                 continue;
             }
+
             numberList.add(i);
         }
         Collections.shuffle(numberList);
         Log.d("TAG", "number List " + numberList.subList(0, 6).toString());
-        return numberList.subList(0, 6);
+        return numberList.subList(0, (6 - pickerNumberSet.size()));
+    }
+
+    private Drawable setTextViewBackground(int number) {
+        Drawable drawable;
+        // Drawable Resource 가져오는 방법
+        // todo 1 ~ 45 까지 색상을 지정해 주세요!!!
+        if (number < 11) {
+            drawable = ContextCompat.getDrawable(this, R.drawable.round_background_1);
+        } else if (number < 21) {
+            drawable = ContextCompat.getDrawable(this, R.drawable.round_background_2);
+        } else {
+            drawable = ContextCompat.getDrawable(this, R.drawable.round_background_1);
+        }
+        return drawable;
     }
 
 
